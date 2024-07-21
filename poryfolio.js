da71d025f1cf6df5575cf4357fc787283129132b
a11-Mir <endos430@gmail.com>
2024-07-22 03:17:28 +0900

    追記

---
+++ poryfolio.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var css = document.createElement("link");
+    css.rel = "stylesheet";
+    css.href = "portfolio.css"; // 実際のCSSファイルのパスを指定する
+    document.head.appendChild(css);
+});
